18843716f607225cdb22d9dbba3049af90ec8932
harishragav963 <132033466+harishragav963@users.noreply.github.com>
2024-06-18 09:37:48 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,46 +1,29 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
-class PerfectSquareFinder {
-
+public class Main {
     public static void main(String[] args) {
-        int start = 1;
-        System.out.println("Enter the Number: ");
         Scanner sc = new Scanner(System.in);
-        int end = sc.nextInt();
-
-        List<Integer> result = findPerfectSquaresWith(start, end, 10);
-
-        System.out.println("Perfect squares in the range [" + start + ", " + end + "] with digit sum less than 10:");
-        for (Integer number : result) {
-            System.out.println(number);
+        System.out.print("ENTER THE SIZE OF THE ARRAY: ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        System.out.print("ENTER THE ELEMENTS IN THE ARRAY: ");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
         }
-    }
 
-    public static List<Integer> findPerfectSquaresWith(int start, int end, int maxDigitSum) {
-        List<Integer> perfectSquares = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        int secMax = Integer.MIN_VALUE;
 
-        for (int i = start; i <= end; i++) {
-            if (isPerfectSquare(i) && sumOfDigits(i) < maxDigitSum) {
-                perfectSquares.add(i);
+        for (int num : a) {
+            if (num > max) {
+                secMax = max;
+                max = num;
+            } else if (num > secMax && num != max) {
+                secMax = num;
             }
         }
 
-        return perfectSquares;
-    }
-
-    public static boolean isPerfectSquare(int number) {
-        int sqrt = (int) Math.sqrt(number);
-        return sqrt * sqrt == number;
-    }
-
-    public static int sumOfDigits(int number) {
-        int sum = 0;
-        while (number > 0) {
-            sum += number % 10;
-            number /= 10;
-        }
-        return sum;
+        System.out.println("THE FIRST MAX IS: " + max);
+        System.out.println("THE SECOND MAX IS: " + secMax);
     }
 }
